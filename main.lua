a0d379f6ef6c84efcf5978937b675f22b1c7b6c4
wakaba (aka. 미카) <kanade.tinkle@gmail.com>
2023-04-22 15:05:10 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -3,11 +3,13 @@ if not EID then return end
 local mod = RegisterMod("Korean EID Descriptions for modded items", 1)
 wakaba_krdesc = mod
 
+-- did this just in case, doesn't include anything yet
 wakaba_krdesc.FIENDFOLIO = include("wakaba_krdesc.fiendfolio")
 wakaba_krdesc.RETRIBUTION = include("wakaba_krdesc.retribution")
 wakaba_krdesc.REVEL = include("wakaba_krdesc.revelations")
 wakaba_krdesc.GODMODE = include("wakaba_krdesc.godmode")
 wakaba_krdesc.SAMAEL = include("wakaba_krdesc.samael")
+wakaba_krdesc.DELIVERANCE = include("wakaba_krdesc.deliverance")
 
 -- Reserve current mod indicator for EID
 EID._currentMod = "Wakaba_translation_reserved"
