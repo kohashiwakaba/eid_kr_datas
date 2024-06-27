71f0a3bf423e21bc5ea888be8add4071fa598e42
wakaba (aka. 미카) <37092106+kohashiwakaba@users.noreply.github.com>
2024-06-27 12:00:44 +0900

    Update sacred_dreams.lua

--- wakaba_krdesc/sacred_dreams.lua
+++ wakaba_krdesc/sacred_dreams.lua
@@ -3,6 +3,7 @@ if SacredDreams then
 
 	EID._currentMod = "Sacred Dreams"
 	local mod = SacredDreams
+	local Guard = SacredDreams:Guard()
 
 	local Items = SDMod.Item
 	local Trinkets = SDMod.Trinket
@@ -13,7 +14,63 @@ if SacredDreams then
 		DECAY = "#부식 상태의 적은 최대 체력에 비례한 지속 피해를 받습니다. (보스의 경우 최대 25)#부식 상태 적 처치 시 적 기준 랜덤한 방향으로 부식성 눈물을 흩뿌립니다.",
 		SLOT_SEL = "{{ButtonRT}}교체 버튼으로 능력을 바꿀 슬롯 선택",
 		SLOT_REPLACE = "{{ArrowGrayUp}} !!!{{ColorOrange}}아래 능력을 위 능력으로 교체합니다.",
+		SOMNO_DURATION = "#{{Somno}} 기면 상태 시간 ",
+		SOMNO_COOLDOWN = "#{{Battery}} Sand Pouch 충전 속도 ",
 	}
+
+	local function sandPouchEIDExtra(type)
+		local durMult = Guard.PouchSomnolenceMultiplier[type]
+		local pouchMult = Guard.PouchChargeMultiplier[type]
+
+		local string1, string2
+		local durArrows, pouchArrows
+		
+		if durMult == 1 then
+				durArrows = "{{ColorYellow}} 유지"
+		elseif durMult > 1 then
+				if durMult >= 1.45 then
+						durArrows = "↑{{BlinkYellowGreen}}대폭 증가{{CR}}"
+				elseif durMult >= 1.15 then
+						durArrows = "↑{{BlinkYellowGreen}}증가{{CR}}"
+				else
+						durArrows = "↑{{BlinkYellowGreen}}소폭 증가{{CR}}"
+				end
+		else
+				if durMult <= 0.55 then
+						durArrows = "↓{{BlinkYellowRed}}대폭 감소{{CR}}"
+				elseif durMult <= 0.85 then
+						durArrows = "↓{{BlinkYellowRed}}감소{{CR}}"
+				else
+						durArrows = "↓{{BlinkYellowRed}}소폭 감소{{CR}}"
+				end
+		end
+		string1 = StatusEffectDesc.SOMNO_DURATION..durArrows
+
+		if pouchMult == 1 then
+				pouchArrows = "{{ColorYellow}} 유지"
+		elseif pouchMult > 1 then
+				if pouchMult >= 1.45 then
+						pouchArrows = "↓{{BlinkYellowRed}}대폭 감소{{CR}}"
+				elseif pouchMult >= 1.20 then
+						pouchArrows = "↓{{BlinkYellowRed}}감소{{CR}}"
+				else
+						pouchArrows = "↓{{BlinkYellowRed}}소폭 감소{{CR}}"
+				end
+		else
+				if pouchMult <= 0.55 then
+						pouchArrows = "↑{{BlinkYellowGreen}}대폭 증가{{CR}}"
+				elseif pouchMult <= 0.80 then
+						pouchArrows = "↑{{BlinkYellowGreen}}증가{{CR}}"
+				else
+						pouchArrows = "↑{{BlinkYellowGreen}}소폭 증가{{CR}}"
+				end
+		end
+		string2 = StatusEffectDesc.SOMNO_COOLDOWN..pouchArrows
+		return {
+			DurationDesc = string1,
+			CooldownDesc = string2
+		}
+	end
 
 	local BirthrightDesc = {
 		[SDMod.PlayerType.PLAYER_GUARD] = {
@@ -23,7 +80,7 @@ if SacredDreams then
 		},
 	}
 	local CollectibleDesc = {
-		[SDMod.Item.BTS] = {
+		[SDMod.Item.BEDTIME_STORY] = {
 			Description = ""
 				.."#사용 시 책을 펼쳐 모래 요정을 소환합니다.#모래 요정은 적에게 기면에 빠지게 합니다."
 				.. "",
@@ -31,7 +88,7 @@ if SacredDreams then
 			QuoteDesc = "착한 아이들은 잠 잘 시간",
 			StatusEffects = {StatusEffectDesc.SOMNO},
 		},
-		[SDMod.Item.BON] = {
+		[SDMod.Item.NIGHTMARE_GRIMOIRE] = {
 			Description = ""
 				.."#사용 시 책을 펼쳐 악몽의 Gaper를 소환합니다."
 				.. "",
@@ -46,7 +103,7 @@ if SacredDreams then
 			QuoteDesc = "마법의 가루가 담겨있어",
 			StatusEffects = {StatusEffectDesc.SOMNO},
 		},
-		[SDMod.Item.SLEEPPILLS] = {
+		[SDMod.Item.SLEEP_PILLS] = {
 			Description = ""
 				.."#스테이지 입장 및 방 6개 클리어 시마다 Sedative 알약을 추가로 드랍합니다."
 				.. "",
@@ -60,14 +117,14 @@ if SacredDreams then
 			Name = "낡은 딸랑이",
 			QuoteDesc = "흔들흔들",
 		},
-		[SDMod.Item.GLASSOFMILK] = {
+		[SDMod.Item.GLASS_OF_MILK] = {
 			Description = ""
-				.."#{{ArrowUp}} {{TearsSmall}}연사 +0.75#{{ArrowDown}} {{ShotspeedSmall}}탄속 -0.15#2.2%의 확률로 {{Confusion}}혼란 or {{Collectible221}}반사 눈물을 발사합니다.#{{LuckSmall}} 행운 40+일 때 100%"
+				.."#↑ {{TearsSmall}}연사 +0.75#↓ {{ShotspeedSmall}}탄속 -0.15#2.2%의 확률로 {{Confusion}}혼란 or {{Collectible221}}반사 눈물을 발사합니다.#{{LuckSmall}} 행운 40+일 때 100%"
 				.. "",
 			Name = "넘치는 우유",
 			QuoteDesc = "연사력 증가 + 발사 속도 감소",
 		},
-		[SDMod.Item.COZYPILLOW] = {
+		[SDMod.Item.COZY_PILLOW] = {
 			Description = ""
 				.."#{{Chargeable}} 충전형 배게 공격을 추가합니다. ({{ButtonRT}}교체 버튼으로 토글)#공격키를 누르고 있으면 충전되며 충전량에 따라 피해량이 증가합니다.#배게로 적 명중 시 깃털이 나옵니다.(충전량 비례)"
 				.. "",
@@ -167,7 +224,7 @@ if SacredDreams then
 			Name = "태아 든 병",
 			QuoteDesc = "",
 		},
-		[SDMod.Item.TEIN] = {
+		[SDMod.Item.TUMOR_HEAD] = {
 			Description = ""
 				.."#확률적으로 부식성 공격이 나갑니다."
 				.. "",
@@ -189,6 +246,13 @@ if SacredDreams then
 			Name = "마스코트 인형",
 			QuoteDesc = "장난감 가게의 흔한 인형",
 		},
+		[SDMod.Item.DREAMERS_SCROLLER] = {
+			Description = ""
+				.."#사용 시 꿈의 오브를 하나 지급합니다.#꿈의 오브 사용 시 랜덤 운세 + Sandy 패밀리어 3~4마리와 아래 중 하나를 드랍합니다: # {{Coin}}동전 3개 #{{SoulHeart}}소울하트 반칸 #{{Card}}카드/룬 #{{Trinket}}장신구"
+				.. "",
+			Name = "꿈의 구슬",
+			QuoteDesc = "미래를 바라봐",
+		},
 	}
 	local TrinketDesc = {
 		[SDMod.Trinket.PLUSHY_SEAL] = {
@@ -208,21 +272,21 @@ if SacredDreams then
 		},
 		[SDMod.Trinket.TOY_CUBE_1] = {
 			Description = ""
-				.."#{{ArrowUp}} {{TearsSmall}}연사 +1#{{Trinket"..SDMod.Trinket.TOY_CUBE_2.."}} 패널티 피격 없이 다음 스테이지 입장 시 Toy Cube N.2로 강화됩니다."
+				.."#↑ {{TearsSmall}}연사 +1#{{Trinket"..SDMod.Trinket.TOY_CUBE_2.."}} 패널티 피격 없이 다음 스테이지 입장 시 Toy Cube N.2로 강화됩니다."
 				.. "",
 			Name = "장난감 큐브 N.1",
 			QuoteDesc = "1을 셀 수 있어!",
 		},
 		[SDMod.Trinket.TOY_CUBE_2] = {
 			Description = ""
-				.."#{{ArrowUp}} {{TearsSmall}}연사 +2# {{ArrowUp}} 공격력 +2#{{Trinket"..SDMod.Trinket.TOY_CUBE_3.."}} 패널티 피격 없이 다음 스테이지 입장 시 Toy Cube N.3로 강화됩니다."
+				.."#↑ {{TearsSmall}}연사 +2# ↑ 공격력 +2#{{Trinket"..SDMod.Trinket.TOY_CUBE_3.."}} 패널티 피격 없이 다음 스테이지 입장 시 Toy Cube N.3로 강화됩니다."
 				.. "",
 			Name = "장난감 큐브 N.2",
 			QuoteDesc = "2를 셀 수 있어!",
 		},
 		[SDMod.Trinket.TOY_CUBE_3] = {
 			Description = ""
-				.."#{{ArrowUp}} {{TearsSmall}}연사 +3# {{ArrowUp}} 공격력 +3# {{ArrowUp}} 사거리 +3"
+				.."#↑ {{TearsSmall}}연사 +3# ↑ 공격력 +3# ↑ 사거리 +3"
 				.. "",
 			Name = "장난감 큐브 N.3",
 			QuoteDesc = "3을 셀 수 있어!",
@@ -240,6 +304,11 @@ local CardDesc = {
 		Name = "페후",
 		QuoteDesc = "풍요",
 	},
+	[SDMod.Consumable.OBJ_ORB] = {
+		Description = "랜덤 운세 + Sandy 패밀리어 3~4마리와 아래 중 하나를 드랍합니다: # {{Coin}}동전 3개 #{{SoulHeart}}소울하트 반칸 #{{Card}}카드/룬 #{{Trinket}}장신구",
+		Name = "꿈의 오브",
+		QuoteDesc = "지혜롭게 물어봐",
+	},
 	--[[ [SamaelMod.ITEMS.SAMAELS_FEATHER] = {
 		Description = "아이템설명",
 		Name = "아이템이름",
@@ -248,7 +317,7 @@ local CardDesc = {
 }
 local PillDesc = {
 	[PillEffect.PILLEFFECT_AMIRMIR] = {
-		Description = "{{ArrowUp}} {{DamageSmall}}공격력 + 3.5#증가한 공격은 23초에 걸쳐 서서히 감소합니다.#{{Somno}} 그 방에서 캐릭터 및 적이 잠에 빠집니다.",
+		Description = "↑ {{DamageSmall}}공격력 + 3.5#증가한 공격은 23초에 걸쳐 서서히 감소합니다.#{{Somno}} 그 방에서 캐릭터 및 적이 잠에 빠집니다.",
 		Name = "진정제",
 		QuoteDesc = "",
 	},
@@ -259,69 +328,140 @@ CollectibleDesc[SDMod.Item.DER_SANDMANN_B] = CollectibleDesc[SDMod.Item.DER_SAND
 local somnDesc = {
 	["Sandplosion"] = {
 		Name = "모래폭발",
-		Description = "Sand Pouch의 범위가 캐릭터 주변으로 변경됩니다.#{{Somno}} 기면 상태 시간 {{BlinkYellowGreen}}증가{{CR}}#{{Battery}} Sand Pouch의 충전 속도 {{BlinkYellowRed}}감소{{CR}}",
+		Description = "Sand Pouch의 범위가 캐릭터 주변으로 변경됩니다.",
 		QuoteDesc = "범위 공격",
 	},
 	["Swiftstep"] = {
 		Name = "재빠른 스텝",
-		Description = "Sand Pouch 사용 시 사용한 방향으로 짧은 거리를 대시합니다. (대시 중 무적)#{{Somno}} 기면 상태 시간 {{BlinkYellowRed}}감소{{CR}}#{{Battery}} Sand Pouch의 충전 속도 {{BlinkYellowGreen}}증가{{CR}}",
+		Description = "Sand Pouch 사용 시 사용한 방향으로 짧은 거리를 대시합니다. (대시 중 무적)",
 		QuoteDesc = "적을 향해 돌진하자!",
 	},
 	["Pouchtoss"] = {
 		Name = "주머니 던지기",
-		Description = "Sand Pouch가 투척 공격으로 바뀝니다.#{{Somno}} 기면 상태 시간 {{BlinkYellowGreen}}소폭 증가{{CR}}#{{Battery}} Sand Pouch의 충전 속도 {{BlinkYellowRed}}소폭 감소{{CR}}",
+		Description = "Sand Pouch가 지형 관통 투척 공격으로 바뀝니다.",
 		QuoteDesc = "휘우우우웅~ 펑!",
 	},
 	["Sparklingsand"] = {
 		Name = "빛나는 모래",
-		Description = "{{Somno}} 기면 상태의 적이 지속시간 동안 총합 캐릭터 x5의 피해를 받습니다.#{{Somno}} 기면 상태 시간 {{BlinkYellowGreen}}소폭 증가{{CR}}#{{Battery}} Sand Pouch의 충전 속도 유지",
+		Description = "{{Somno}} 기면 상태의 적이 지속시간 동안 총합 캐릭터 x5의 피해를 받습니다.",
 		QuoteDesc = "졸음은 위험하다구!",
 	},
 	["Comacloud"] = {
 		Name = "코마 구름",
-		Description = "Sand Pouch 사용 시 적에게 피해를 주는 코마 구름을 추가로 소환합니다.#코마 구름에 있는 적은 기면 상태에서 벗어나지 못하며 멈추게 됩니다.#{{Somno}} 기면 상태 시간 {{BlinkYellowRed}}소폭 감소{{CR}}#Sand Pouch의 충전 속도 {{BlinkYellowRed}}소폭 감소{{CR}}",
+		Description = "Sand Pouch 사용 시 적에게 피해를 주는 코마 구름을 추가로 소환합니다.#코마 구름에 있는 적은 기면 상태에서 벗어나지 못하며 멈추게 됩니다.",
 		QuoteDesc = "더 오랫동안 잠들게",
 	},
 	["Sandy"] = {
 		Name = "Sandy!",
-		Description = "{{Somno}} 기면 상태 적 처치 시 적을 따라다니며 기면 상태에 걸리게 하는 자폭 모래 요정을 추가로 소환합니다.#{{Somno}} 기면 상태 시간 유지#{{Battery}} Sand Pouch의 충전 속도 {{BlinkYellowGreen}}소폭 증가{{CR}}",
+		Description = "{{Somno}} 기면 상태 적 처치 시 적을 따라다니며 기면 상태에 걸리게 하는 자폭 모래 요정을 추가로 소환합니다.",
 		QuoteDesc = "수면 친구!",
 	},
 	["Fairytrail"] = {
 		Name = "요정의 흔적",
-		Description = "Sand Pouch의 공격 범위에 적에게 둔화 피해를 주는 장판을 설치합니다.#{{Somno}} 기면 상태 시간 유지#{{Battery}} Sand Pouch의 충전 속도 유지",
+		Description = "Sand Pouch의 공격 범위에 적에게 둔화 피해를 주는 장판을 설치합니다.",
 		QuoteDesc = "노란 모랫길",
 	},
 	["Oversomnia"] = {
 		Name = "과수면증",
-		Description = "{{Somno}} 기면 상태 중첩 가능 (최대 3)#!!! 중첩 당 추가 피해와 기면 상태 쿨타임 증가#{{Somno}} 기면 상태 시간 유지#{{Battery}} Sand Pouch의 충전 속도 {{BlinkYellowRed}}소폭 감소{{CR}}",
+		Description = "{{Somno}} 기면 상태 중첩 가능 (최대 3)#!!! 중첩 당 추가 피해와 기면 상태 쿨타임 증가",
 		QuoteDesc = "못 일어나겠어...",
 	},
 	["Shootingstar"] = {
 		Name = "슈팅 스타",
-		Description = "Sand Pouch 사용 시 별조각을 추가로 발사합니다.#{{Somno}} 기면 상태 시간 {{BlinkYellowRed}}감소{{CR}}#{{Battery}} Sand Pouch의 충전 속도 {{BlinkYellowGreen}}증가{{CR}}",
+		Description = "Sand Pouch 사용 시 별조각을 추가로 발사합니다.",
 		QuoteDesc = "소원을 빌자!",
 	},
 	["Deepsleep"] = {
 		Name = "깊은 잠",
 		Description = "#{{Somno}} 기면 상태 시간 {{BlinkYellowGreen}}50% 증가{{CR}}#{{Battery}} Sand Pouch의 충전 속도 {{BlinkYellowGreen}}소폭 증가{{CR}}",
 		QuoteDesc = "기면 시간 증가",
+		ExcludeExtra = true,
 	},
 	["Dreamwarrior"] = {
 		Name = "꿈의 전사",
-		Description = "{{Somno}} 기면 상태의 적에게 25% 추가 피해를 줍니다.#{{Somno}} 기면 상태 시간 유지#{{Battery}} Sand Pouch의 충전 속도 유지",
+		Description = "{{Somno}} 기면 상태의 적 처치 시 그 방에서 기면 상태의 적에게 5% 추가 피해를 줍니다. (최대 50%)",
 		QuoteDesc = "기면 피해 증가",
 	},
 	["Spindlespikes"] = {
 		Name = "스핀들 가시",
-		Description = "Sand Pouch를 사용한 위치에 모래 가시를 추가로 소환합니다.#{{Somno}} 기면 상태 시간 유지#{{Battery}} Sand Pouch의 충전 속도 유지",
+		Description = "Sand Pouch를 사용한 위치에 모래 가시를 추가로 소환합니다.",
 		QuoteDesc = "손이 찔리지 않도록 조심해",
 	},
 	["Somnolepticcharge"] = {
 		Name = "수면 충전",
-		Description = "{{Somno}} 기면 상태의 적 명중 시 주머니를 충전시킵니다.#{{Somno}} 기면 상태 시간 {{BlinkYellowRed}}소폭 감소{{CR}}#{{Battery}} Sand Pouch의 충전 속도 {{BlinkYellowRed}}대폭 감소{{CR}}",
+		Description = "{{Somno}} 기면 상태의 적 명중 시 주머니를 충전시킵니다.#Sand Pouch 추가 충전 가능",
 		QuoteDesc = "주머니의 기력을 모으자!",
 	},
+	["Sturdystrike"] = {
+		Name = "묵직한 공격",
+		Description = "Sand Pouch 3번째 사용 시마다 추가 공격을 날립니다.",
+		QuoteDesc = "더 많은 모래",
+	},
+	["Mesmericmortar"] = {
+		Name = "메즈메릭 박격포",
+		Description = "Q 버튼을 꾹 누르면 주변의 적을 공격하는 대포를 발사합니다. #적의 탄환을 막아주며 너무 많이 맞으면 사라집니다.",
+		QuoteDesc = "",
+	},
+	["Magicmissile"] = {
+		Name = "매직 미사일",
+		Description = "공격키를 2번 누르면 사거리가 짧은 적 관통 구체를 발사합니다.#구체는 기면 상태의 적에게 추가 피해를 줍니다.#{{Blank}} (구체는 별도의 충전량을 가짐)",
+		QuoteDesc = "더블탭 글레이즈",
+	},
+	["Carelesscarousel"] = {
+		Name = "무모한 회오리",
+		Description = "Sand Pouch의 공격이 여러번 회전하는 회전 공격으로 바뀝니다.",
+		QuoteDesc = "빙글빙글",
+	},
+	["Cloggyconvolution"] = {
+		Name = "콘벌루션 차단",
+		Description = "일반 적의 기면 면역 제거#보스의 기면 면역 시간 -75%",
+		QuoteDesc = "기면 면역 차단",
+	},
+	["HourglassHaste"] = {
+		Name = "재빠른 모래시계",
+		Description = "모래 게이지 충전량 +75%",
+		QuoteDesc = "모래 게이지 충전량 증가",
+	},
+	["Contagiousyawn"] = {
+		Name = "전염성 하품",
+		Description = "기면 상태의 적이 확률적으로 주변의 적을 기면에 빠지게 합니다.#{{Blank}} (각 적 당 한번만 발동 가능)",
+		QuoteDesc = "",
+	},
+	["Somnambula"] = {
+		Name = "Somnambula",
+		Description = "가장 가까운 적을 지정합니다.#지정된 적은 기면 상태일 때 추가 피해 +35%",
+		QuoteDesc = "그들을 깨우지 마!",
+	},
+	["Outburst"] = {
+		Name = "아웃버스트",
+		Description = "기면 상태의 적 명중 시 다른 적을 공격하는 영혼을 소환합니다.#영혼이 적 명중 시 짧은 기면 상태에 빠지게 합니다.",
+		QuoteDesc = "모두 꿈나라로",
+	},
+	["Prettypocketpack"] = {
+		Name = "예쁜 주머니 팩",
+		Description = "{{Warning}} 포인트 소모 없음 #포인트를 소모하지 않는 업그레이드를 최대 3개 소환합니다.#{{Blank}} (획득 시 즉시 사용, 한 게임 당 1회만 획득 가능)",
+		QuoteDesc = "선택은 신중히",
+	},
+	["Bedtimebundle"] = {
+		Name = "베드타임 번들",
+		Description = "{{Warning}} 포인트 소모 없음 #스테이지 진입 시 업그레이드 선택지 +1",
+		QuoteDesc = "더 많은 능력",
+	},
+	["Coupon"] = {
+		Name = "쿠폰",
+		Description = "{{Warning}} 포인트 소모 없음 #업그레이드 하나를 무료로 획득할 수 있습니다. (사용 시 소모)",
+		QuoteDesc = "내 이름이 있어!",
+	},
+	["Freshstart"] = {
+		Name = "신선한 시작",
+		Description = "{{Warning}} 포인트 소모 없음 #모든 업그레이드를 제거하고 업그레이드에 사용한 포인트를 반환합니다.",
+		QuoteDesc = "태초마을이야!",
+	},
+
+
+
+
+
 }
 EID:addEntity(1000, SDMod.SOMNOSSENCE, 0, "수면", "음냐음냐", "ko_kr")
 
@@ -384,10 +524,15 @@ local function FF_EIDKR_SSDescCallback(descObj)
 	local ss = e:GetSprite()
 	local sd = e:GetData()
 	local ov = ss:GetOverlayAnimation()
+	local g = SacredDreams:Guard()
 	if somnDesc[ov] then
 		local entry = somnDesc[ov]
 		descObj.Name = entry.Name
 		descObj.Description = entry.Description
+		if not entry.ExcludeExtra then
+			local exDesc = sandPouchEIDExtra(g.ItemToSlot[ov])
+			descObj.Description = descObj.Description .. exDesc.DurationDesc .. "" .. exDesc.CooldownDesc
+		end
 
 		local p = EID.player
 		local d = p:GetData()
@@ -396,7 +541,6 @@ local function FF_EIDKR_SSDescCallback(descObj)
 		if pos == 0 or (slot and slot == 0) then
 			EID:appendToDescription(descObj, "#{{ColorOrange}}"..StatusEffectDesc.SLOT_SEL)
 		elseif slot then
-			local g = SacredDreams:Guard()
 			for k, v in pairs(g.ItemToSlot) do
 				if v == slot then
 					local entry2 = somnDesc[k]
@@ -415,6 +559,48 @@ local function FF_EIDKR_SSDescCallback(descObj)
 end
 
 EID:addDescriptionModifier("FF_EIDKR_Somni", FF_EIDKR_SSCondition, FF_EIDKR_SSDescCallback)
+
+local function FF_EIDKR_SPCondition(descObj)
+	if EID:getLanguage() ~= "ko_kr" then return false end
+	--if (EID.InsideItemReminder) then return false end
+
+	local num_players = Game():GetNumPlayers()
+	for i=1,(num_players) do
+		local p = Isaac.GetPlayer(i)
+		local d = p:GetData()
+		if not p:GetPlayerType() == SDMod.PlayerType.PLAYER_GUARD then
+			return false
+		end
+		if not p:HasCollectible(SDMod.Item.SAND_POUCH) then
+			return false
+		end
+		if d.SDGuardData and d.SDGuardData.SelectedSlot ~= 0 then
+			if descObj.ObjType == 5 and descObj.ObjVariant == 100 and descObj.ObjSubType == SDMod.Item.SAND_POUCH then return true end
+		end
+	end
+end
+
+local function FF_EIDKR_SPDescCallback(descObj)
+	local num_players = Game():GetNumPlayers()
+	for i=1,(num_players) do
+		local p = Isaac.GetPlayer(i)
+		local d = p:GetData()
+		if d.SDGuardData and d.SDGuardData.SelectedSlot ~= 0 then
+			local slot = d.SDGuardData.PouchInventory[d.SDGuardData.SelectedSlot]
+			local g = SacredDreams:Guard()
+			local keyStr = g.UpgradeOverlay[slot]
+			if keyStr and somnDesc[keyStr] then
+				descObj.Description = string.gsub(descObj.Description, "(#{{Player"..SDMod.PlayerType.PLAYER_GUARD..").+", "")
+				local name = somnDesc[keyStr].Name
+				local desc = somnDesc[keyStr].Description
+				EID:appendToDescription(descObj, "#{{Player"..SDMod.PlayerType.PLAYER_GUARD.."}} {{ColorObjName}}".. name.."#"..desc)
+				return descObj
+			end
+		end
+	end
+	return descObj
+end
+EID:addDescriptionModifier("FF_EIDKR_SandPouch", FF_EIDKR_SPCondition, FF_EIDKR_SPDescCallback)
 
 if Options.Language == "kr" then
 	local g = SacredDreams:Guard()
