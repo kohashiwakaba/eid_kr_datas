a1dafeb688dd658ebf12255d763d2533d5a1d525
wakaba (aka. 미카) <kanade.tinkle@gmail.com>
2024-07-21 21:41:32 +0900

    Update sheriff.lua

--- wakaba_krdesc/sheriff.lua
+++ wakaba_krdesc/sheriff.lua
@@ -1,11 +1,11 @@
 
 if Sheriff then
 
-	EID._currentMod = "The Sheriff"
-
+	EID._currentMod = "TSC Unlock API (TheSheriff)"
 
 	local Items = Sheriff.Items
 	local Trinkets = Sheriff.Items
+	local Cards = Sheriff.PocketItems
 
 	local BirthrightDesc = {
 		[Sheriff.Characters.TheSheriff.CHARACTER_ID] = {
@@ -13,11 +13,16 @@ if Sheriff then
 			Description = "{{SheriffBullet}} 적 처치 시 확률적으로 탄창 픽업을 드랍합니다.#{{SheriffBullet}} 탄창을 주우면 탄환 +3 (초과 보유 가능)",
 			QuoteDesc = "빼앗고 다시 쓰기",
 		},
+		[Sheriff.Characters.TaintedSheriff.CHARACTER_ID] = {
+			Name = "Tainted Sheriff",
+			Description = "저격되지 않은 적을 채찍으로 명중 시 특수 저격 표식이 생깁니다.#특수 저격 상태의 적이 1초마다 특수 탄창을 드랍합니다. (초과 보유 가능)",
+			QuoteDesc = "쥐어짜내기",
+		},
 	}
 	local CollectibleDesc = {
 		[Items.ArmedBombs.ID] = {
 			Name = "조준 폭탄",
-			Description = "↑ {{Bomb}}폭탄 +5#적이 있는 방에서 폭탄이 더 느리게 터집니다.#폭탄이 적을 향해 총탄을 발사하며, 폭탄이 터지면 5방향으로 총탄을 발사합니다.",
+			Description = "↑ {{Bomb}}폭탄 +5#적이 있는 방에서 폭탄이 더 느리게 터집니다.#{{SheriffBullet}} 폭탄이 적을 향해 총탄을 발사하며, 폭탄이 터지면 5방향으로 총탄을 발사합니다.",
 			QuoteDesc = "총알 폭발 폭탄 +5개",
 		},
 		[Items.BanditsBandana.ID] = {
@@ -47,7 +52,7 @@ if Sheriff then
 		},
 		[Items.MOAB.ID] = {
 			Name = "B.U.R.T.",
-			Description = "사용 시 일정 시간 후 거대 폭탄이 떨어집니다. (자해 없음)#{{SheriffBullet}} 폭발 시 적에게 1666의 방어 무시 피해를 주며 무언가에 반사되는 추가 총탄을 여러 발 발사합니다.",
+			Description = "!!! 일회용#사용 시 일정 시간 후 거대 폭탄이 떨어집니다. (자해 없음)#{{SheriffBullet}} 폭발 시 적에게 1666의 방어 무시 피해를 주며 무언가에 반사되는 추가 총탄을 여러 발 발사합니다.",
 			QuoteDesc = "Bullet Unleashing Righteous Terror",
 		},
 		[Items.QuickDraw.ITEM_ID] = {
@@ -65,12 +70,77 @@ if Sheriff then
 			Description = "방 안에 적에게 피해를 주는 회전초가 굴러다닙니다.#{{DamageSmall}} 회전초의 피해량은 캐릭터 공격력에 비례",
 			QuoteDesc = "어디서 오는 거야?!",
 		},
+		[Items.Holster.ID] = {
+			Name = "홀스터",
+			Description = "사용 시 공격이 휘두를 수 있는 채찍으로 변경됩니다."
+			.."#채찍은 적에게 공격력 x0.5의 저격 피해를 줍니다."
+			.."#저격된 적 명중 시 탄창을 드랍합니다.",
+			QuoteDesc = "맞추고 정복해라",
+			NonSheriffDescription = "사용 시 공격이 휘두를 수 있는 채찍으로 변경됩니다."
+			.."#채찍은 적에게 공격력 x0.7의 둔화 피해를 줍니다.",
+		},
+		[Items.GlassBullets.ID] = {
+			Name = "유리 총알",
+			Description = "적 탄환을 가까스로 피하면 캐릭터의 텐션pt가 증가합니다."
+			.."#{{Blank}} (캐릭터 아웃라인 및 효과음으로 확인)"
+			.."#{{SheriffBullet}} 텐션pt가 5 이상일 때 공격키를 두번 눌러 공격력 x3의 커다란 총알을 발사합니다."
+			.."#!!! 피격 시 텐션 pt가 사라짐",
+			QuoteDesc = "무모함의 보상, 잃지 마!",
+		},
+		[Items.SpiritOfTheWest.ID] = {
+			Name = "서부의 정령",
+			Description = "주기적으로 나왔다가 사라집니다."
+			.."#랜덤 적 근처에 나오며 적에게 1.66의 관통 총알을 5발 발사합니다.",
+			QuoteDesc = "승리는 당신을 향해 미소를 지어",
+		},
+		[Items.LightningRod.ID] = {
+			Name = "피뢰침",
+			Description = "소지 중일 때 비가 내리며 주기적으로 랜덤 위치에 100의 피해를 주는 번개가 내리칩니다."
+			.."#사용 시 그 자리에 피뢰침을 설치하여 주기적으로 그 위치에 200의 피해를 주는 번개를 내리칩니다. (최대 3회)"
+			.."#{{Blank}} (번개는 캐릭터에게 피해를 주지 않음)",
+			QuoteDesc = "장족의 기술 발전",
+		},
 	}
+
+	CollectibleDesc[Items.Holster.WHIP_VERSION_ID] = CollectibleDesc[Items.Holster.ITEM_ID]
+
 	local TrinketDesc = {
 		[Items.OilLamp.ID] = {
 			Name = "오일램프",
-			Description = "{{CurseDarknessSmall}} Darkness 저주에 걸리지 않습니다.#보스방으로 향하는 위치로 날아갑니다.",
+			Description = "{{BossRoom}} 보스방으로 향하는 위치로 날아갑니다.",
 			QuoteDesc = "암흑 속의 빛",
+		},
+		[Items.GoldDust.ID] = {
+			Name = "금빛 먼지",
+			Description =
+			"{{Shop}} 상점 주인이 서서히 황금화 되며;"
+			.."#{{MiniBoss}} 완전 황금화 되면 Greed 미니보스로 변신합니다."
+			.."#!!! 방을 나가면 무효화",
+			QuoteDesc = "탐욕은 쉬지를 않지",
+		},
+		[Items.SeedPouch.ID] = {
+			Name = "씨앗 주머니",
+			Description = "적이 있는 방에서 주기적으로 무 줄기가 생깁니다."
+			.."#캐릭터가 무를 들고 던질 수 있으며 던진 무는 적에게 캐릭터의 공격력 x2의 관통 피해를 줍니다.",
+			QuoteDesc = "GMO는 우리의 M.O.!",
+		},
+	}
+	local CardDesc = {
+		[Cards.CasinoChip.ID] = {
+			Name = "카지노 칩",
+			Description = "{{Coin}} 사용 시 그 방의 동전을 50%의 확률로 업그레이드하거나 다운그레이드 시킵니다.",
+			QuoteDesc = "돈 2배?",
+		},
+		[Cards.SoulOfSheriff.ID] = {
+			Name = "셰리프의 영혼",
+			Description = "{{SheriffBullet}} 10초동안 적을 향해 리볼버를 난사합니다.",
+			QuoteDesc = "정의는 승리하리라",
+		},
+		[Cards.CancerPack.ID] = {
+			Name = "암 패키지",
+			Description = "{{Timer}} 사용 시 1분동안 캐릭터의 자리에 담배 연기를 피웁니다."
+			.."#{{Confusion}} 담배 연기는 초당 캐릭터의 공격력 x2의 혼란 피해를 줍니다.",
+			QuoteDesc = "흡연구역",
 		},
 	}
 
@@ -103,13 +173,45 @@ if Sheriff then
 		end
 		EID:addTrinket(itemID, desc, itemdesc.Name, "ko_kr")
 	end
+	for itemID, itemdesc in pairs(CardDesc) do
+		EID:addCard(itemID, itemdesc.Description, itemdesc.Name, "ko_kr")
+	end
+
+	local function modifyHolsterCondition(descObj)
+		if EID:getLanguage() ~= "ko_kr" then return false end
+		return descObj.ObjType == 5
+		and descObj.ObjVariant == 100
+		and (descObj.ObjSubType == Sheriff.Items.Holster.ID or descObj.ObjSubType == Sheriff.Items.Holster.WHIP_VERSION_ID)
+	end
+
+	local function modifyHolsterCallback(descObj)
+		local anySheriff = false
+		for i = 0, Sheriff.Game:GetNumPlayers() - 1 do
+			local player = Isaac.GetPlayer(i)
+			if player:GetPlayerType() == Sheriff.Characters.TaintedSheriff.CHARACTER_ID then
+				anySheriff = true
+				break
+			end
+		end
+
+		if not anySheriff then
+			local descriptions = CollectibleDesc[Sheriff.Items.Holster.ID].NonSheriffDescription
+			descObj.Description = descriptions
+		end
+
+		return descObj
+	end
+
+	EID:addDescriptionModifier("FF_EIDKR_Holster Alternate Description", modifyHolsterCondition, modifyHolsterCallback)
+
 
 
 	return {
-		targetMod = "The Sheriff",
+		targetMod = "TSC Unlock API (TheSheriff)",
 		birthright = BirthrightDesc,
 		collectibles = CollectibleDesc,
 		trinkets = TrinketDesc,
+		cards = CardDesc,
 	}
 
 end
