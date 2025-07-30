a48e324df9a6f7a8e55dfa0a1225bd979148c1e7
리셰/richer-chan <kanade.tinkle@gmail.com>
2025-07-30 23:20:43 +0900

    Update heavens_call.lua

--- wakaba_krdesc/heavens_call.lua
+++ wakaba_krdesc/heavens_call.lua
@@ -77,427 +77,252 @@ if HeavensCall then
 
 		--#region SOLAR ITEMS
 		[mod.SolarItems.HyperDice] = {
-			Name = "아브라함의 지도",
+			Name = "하이퍼 주사위",
 			Description = ""
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
+			.."#특수방 근처에서 사용 시 그 방을 다른 특수방으로 바꿉니다."
+			.."#!!! 이미 들어간 방에는 사용 불가"
+			.."{{CR}}",
+			QuoteDesc = "공간을 바꾸는 능력",
+		},
+		[mod.SolarItems.SpaceJam] = {
+			Name = "우주 잼",
+			Description = ""
+			.."#하늘에서 과일이 떨어집니다."
+			.."#과일 획득 시 증발성 능력치 증가"
+			.."#과일이 땅에 닿을 때 주변의 적에게 {{DamageSmall}}/{{TearsSmall}} 비례 폭발 피해를 줍니다."
+			.."{{CR}}",
+			QuoteDesc = "스타베리",
+		},
+		[mod.SolarItems.AsteroidBelt] = {
+			Name = "소행성 벨트",
+			Description = ""
+			.."#{{Coin}}/{{Bomb}}/{{Key}} 각각 20개마다:"
+			.."#>>> 각 픽업별 공전형 패밀리어를 얻습니다."
+			.."{{CR}}",
+			QuoteDesc = "우주의 허리",
+		},
+		[mod.SolarItems.Stallion] = {
+			Name = "헤랄드의 종마",
+			Description = ""
+			.."#종마 근처에서 {{ButtonRT}} 버튼을 꾹 누르면 탑승합니다."
+			.."#!!! {{ColorTransform}}탑승 중일 때:"
+			.."#>>> {{SpeedSmall}}+0.25/가시,장판면역/장애물파괴"
+			.."#>>> 돌진 시 적에게 피해"
+			.."#!!! {{ColorTransform}}탑승 중이지 않을 때:"
+			.."#>>> 적 경로 방해 및 탄환 방어"
+			.."#>>> 방귀 및 독구름 생성"
+			.."#>>> 종마를 밀어 움직일 수 있음"
+			.."{{CR}}",
+			QuoteDesc = "[버리기]를 꾹 눌러 타자",
+		},
+		[mod.SolarItems.Panspermia] = {
+			Name = "팬스페르미아",
+			-- "#{{ArrowUp}} {{Heart}} +1 Health # Damaging enemies charges blood # Double tap the attack button to release a flesh whip that does heavy damage, consuming one blood charge # Overcharging blood consumes it all and drops half a red heart #{{HealingRed}} 25% chance to heal half a heart every 30 seconds"
+			Description = ""
+			.."#↑ {{Heart}}최대 체력 +1"
+			.."#적 명중 시 심장 게이지를 충전합니다. (최대 3)"
+			.."#!!! 공격 키를 두 번 눌러 심장 게이지를 1칸 소모하고:"
+			.."#>>> 공격방향으로 적에게 최대 90의 피해를 주는 채찍을 휘두릅니다."
+			.."#!!! 심장 게이지 초과 충전 시 전부 소진 후 {{HalfHeart}}를 드랍합니다."
+			.."#{{HealingRed}} 30초마다 25%의 확률로 빨간하트 반칸을 회복합니다."
+			.."{{CR}}",
+			QuoteDesc = "피의 그릇",
+		},
+		[mod.SolarItems.Theia] = {
+			Name = "테이아",
+			-- "# Grants a large Theia familiar chained to the player # Theia leaps at enemies to attack and may occasionally target the player # When idle, the player can pull her by moving # While attacking, the player is dragged by the chain # Theia breaks rocks #{{Rune}} Holding a rune grants Theia special abilities"
+			Description = ""
+			.."#캐릭터와 연결되어 이동하며 접촉한 장애물을 부숩니다."
+			.."#접촉한 적에게 초당 30의 피해를 줍니다."
+			.."#근처의 적을 향해 돌진하며;"
+			.."#>>> !!! 캐릭터도 같이 끌려갑니다."
+			.."#!!! 2%의 확률로 적이 아닌 캐릭터를 공격합니다. ({{HalfHeart}}, 패널티 미발동)"
+			.."#{{Rune}} 룬 소지 중일 때 ???"
+			.."{{CR}}",
+			QuoteDesc = "아틀라스의 족쇄",
+		},
+		[mod.SolarItems.Engine] = {
+			Name = "로켓 엔진",
+			-- "# Replaces Isaac's attack with a continuous short-range flame # The engine overheats if used too long, disabling the flame until it cools down # Higher {{Tears}} increases flame duration before overheating # Higher {{Shotspeed}} speeds up cooling"
+			Description = ""
+			.."#{{Chargeable}} 공격이 짧은 불길 레이저로 바뀝니다."
+			.."#{{DamageSmall}} 불길은 적에게 초당 캐릭터의 공격력 x10의 피해를 줍니다."
+			.."#!!! 오랫동안 공격 시 일정 시간동안 공격 불가"
+			.."#{{TearsSmall}} 연사에 비례하여 공격 가능 지속시간 증가"
+			.."#{{ShotspeedSmall}} 탄속에 비례하여 충전속도 증가"
+			.."{{CR}}",
+			QuoteDesc = "오버드라이브!",
+		},
+		[mod.SolarItems.Battery] = {
+			Name = "천체 배터리",
+			-- "# Using an active item discharges it and spawns an {{ColorTransform}}item spark{{ColorText}} # Item sparks behave like active item wisps, triggering the effect when destroyed # If the active item is used without enough charge, the oldest spark is consumed instead"
+			Description = ""
+			.."#{{Battery}} 액티브 아이템 사용 시도 시:"
+			.."#>>> 아이템 발동을 무효화하며;"
+			.."#>>> 아이템 스파크를 하나 소환합니다."
+			.."#완충 상태가 아닐 때 스파크를 소모하여 액티브 아이템을 사용할 수 있습니다."
+			.."{{CR}}",
+			QuoteDesc = "별빛 키우기",
+		},
+		[mod.SolarItems.Mothership_01] = {
+			Name = "수정 모듈",
+			-- "#{{1}} Fires a laser that infects enemies; infected enemies spawn friendly maggots on death #{{2}} Infected enemies become charmed #{{3}} Isaac can keep 2 additional maggots between rooms # Increases power when combined with other Mothership modules # Other Mothership modules appear more often", "The Inseminator"
+			Description = ""
+			.."#!!! 모듈 소지 종류에 따라 효과 강화:"
+			.."#{{1}} 적에게 감염성 레이저 발사 + 감염된 적 처치 시 아군 Maggot 소환"
+			.."#{{2}} {{Charm}} 감염된 적 매혹"
+			.."#{{3}} 방 입장 시 최대 2마리의 Maggot 유지"
+			.."#!!! 소지 중일 때 모듈 등장 확률 증가"
+			.."{{CR}}",
+			QuoteDesc = "생명 파츠",
+		},
+		[mod.SolarItems.Mothership_02] = {
+			Name = "권총 모듈",
+			-- "#{{1}} Automatically fires at nearby enemies #{{2}} Increased damage and spectral tears #{{3}} Rocket launcher # Increases power when combined with other Mothership modules # Other Mothership modules appear more often", "The Pistol"
+			Description = ""
+			.."#!!! 모듈 소지 종류에 따라 효과 강화:"
+			.."#{{1}} 적을 향해 자동으로 눈물 발사 ({{Damage}}:2 x6)"
+			.."#{{2}} {{Damage}}:4 x9 / 장애물 관통"
+			.."#{{3}} 로켓을 추가로 발사 ({{Damage}}:50/자해 없음)"
+			.."#!!! 소지 중일 때 모듈 등장 확률 증가"
+			.."{{CR}}",
+			QuoteDesc = "공격 파츠",
+		},
+		[mod.SolarItems.Mothership_03] = {
+			Name = "관통 모듈",
+			-- "#{{1}} Destroys tinted and golden rocks #{{2}} Opens secret rooms and stone chests #{{2}} Destroys spiked rocks and Bishops #{{3}} Opens crawlspaces # Increases power when combined with other Mothership modules # Other Mothership modules appear more often", "The Penetrator"
+			Description = ""
+			.."#!!! 모듈 소지 종류에 따라 효과 강화:"
+			.."#{{1}} 색돌, 황금돌 파괴"
+			.."#{{2}} {{SecretRoom}} 비밀방, 돌상자 개방"
+			.."#{{2}} 가시돌, Bishop 파괴"
+			.."#{{3}} {{LadderRoom}} 사다리방으로 가는 다락문 생성"
+			.."#!!! 소지 중일 때 모듈 등장 확률 증가"
+			.."{{CR}}",
+			QuoteDesc = "지원 파츠",
+		},
+		[mod.SolarItems.Mothership_04] = {
+			Name = "보호 모듈",
+			-- "#{{1}} Grants a shield every 5 rooms #{{2}} Grants a shield every 3 rooms #{{2}} Blocks projectiles #{{3}} Isaac can stack two shields # Increases power when combined with other Mothership modules # Other Mothership modules appear more often", "The Protector"
+			Description = ""
+			.."#!!! 모듈 소지 종류에 따라 효과 강화:"
+			.."#{{1}} 방 5개 클리어 시 피해 무효 +1회 (최대 1)"
+			.."#{{2}} 방 3개 클리어 시 피해 무효 +1회 (최대 1)"
+			.."#{{2}} 모선이 적 탄환을 막아줍니다."
+			.."#{{3}} 방 3개 클리어 시 피해 무효 +1회 (최대 2)"
+			.."#!!! 소지 중일 때 모듈 등장 확률 증가"
+			.."{{CR}}",
+			QuoteDesc = "보호 파츠",
+		},
+		[mod.SolarItems.Telescope] = {
+			Name = "황금 망원경",
+			-- "# When leaving a floor, all unvisited rooms convert into bonus golden rooms on the next floor # Golden rooms are lower weight than standard rooms # Large rooms count as multiple # Unvisited golden rooms also carry over #{{ArrowUp}} +2 Luck", "Golden Telescope"
+			Description = ""
+			.."#↑ {{LuckSmall}}행운 +2"
+			.."#가능한 경우, 스테이지 진입 시 이전 스테이지의 방문하지 않은 방의 수만큼 황금의 방을 추가로 생성합니다."
+			.."#황금의 방은 일반 방에 비해 희귀한 구조가 더 많이 등장합니다."
+			.."{{CR}}",
+			QuoteDesc = "더 많은 방",
+		},
+		[mod.SolarItems.Quasar] = {
+			Name = "퀘이사",
+			-- "# On use, if collectibles are present in the room, consumes no charges and absorbs them # Each absorbed item grants charges based on its quality + 1 x4 # On use, if no collectibles are present, creates two twin giant lasers for a few seconds, consuming one charge #{{Warning}} This item can only be recharged by absorbing collectibles", "Quasar"
+			Description = ""
+			.."#!!! 방 클리어로 충전 불가"
+			.."#{{Battery}} 사용 시:"
+			.."#>>> 그 방의 아이템을 흡수하여 (등급+1)*4만큼 충전합니다."
+			.."#{{Battery}} 그 방에 아이템이 없을 때 사용 시 충전량 1칸을 소모하여;"
+			.."#>>> 2초간 양쪽으로 거대 레이저를 발사, 적에게 최대 3500의 피해를 줍니다."
+			.."{{CR}}",
+			QuoteDesc = "천체 파괴자",
+		},
+		[mod.SolarItems.RedShovel] = {
+			Name = "붉은 삽",
+			-- "# On use, Isaac enters a red trapdoor leading to a parallel floor at the same progression point # While on the red floor, the shovel becomes {{Collectible84}} and reverts after completing the floor", "Red Shovel"
+			Description = ""
+			.."#{{Battery}} 사용 시:"
+			.."#>>> 빨간 트랩도어를 소환합니다."
+			.."#빨간 트랩도어 입장 시 스테이지를 재시작합니다."
+			.."#>>> !!! 진행 루트가 바뀔 수 있음"
+			.."#{{RedRoom}} 재시작한 스테이지에서는 삽이 {{Collectible84}}로 바뀌며 모든 방이 빨간방으로 설정됩니다."
+			.."#다음 스테이지 진입 시 삽이 복원됩니다."
+			.."{{CR}}",
+			QuoteDesc = "토끼굴로",
+		},
+		[mod.SolarItems.Picatrix] = {
+			Name = "피카트릭스",
+			-- "# On use, spawns an astral image of the player that orbits around them # The astral image has all the player's original items as item wisps and attacks simultaneously with the player # The astral image disappears when changing rooms or if all wisps are lost", "Picatrix"
+			Description = ""
+			.."#{{Room}} 사용 시 그 방에서 환영을 소환합니다."
+			.."#>>> 환영은 캐릭터 주변을 돌며 공격방향으로 캐릭터의 공격과 같은 공격을 발사합니다."
+			.."#>>> 환영 소환 시 캐릭터가 소지한 아이템 위습을 같이 할당합니다."
+			.."#할당된 아이템 위습이 전부 사라지거나 방 이동 시 환영이 사라집니다."
+			.."{{CR}}",
+			QuoteDesc = "별의 수호자",
+		},
+		[mod.SolarItems.LilSol] = {
+			Name = "리틀 솔",
+			-- "# Grants a Lil Sol familiar that bounces around the room # Has an aura similar to {{Collectible417}}, behaving like the one from {{Collectible543}} # All Lil Moon familiars will orbit Lil Sol instead of Isaac", "Lil Sol"
+			Description = ""
+			.."#{{Collectible417}} 흰 오라를 가지고 대각선으로 이동합니다."
+			.."#{{Collectible543}} 오라 안에 들어온 캐릭터의 공격력 x1.2, 연사 x2.5"
+			.."#모든 위성 패밀리어가 Lil Sol 주변을 돕니다."
+			.."{{CR}}",
+			QuoteDesc = "무적의 태양",
+		},
+		[mod.SolarItems.Mochi] = {
+			Name = "모찌",
+			-- "# {{ArrowUp}} +1 Health # {{HealingRed}} Heals 1 heart # {{ArrowUp}} +0.25 Tears # {{ArrowUp}} +0.25 Damage # {{ArrowUp}} +1 Luck # {{LunarHC}} x2 Lunar Pact chance", "Mochi"
+			Description = ""
+			.."#↑ {{Heart}}최대 체력 +1"
+			.."#↑ {{HealingRed}}빨간하트 +1"
+			.."#↑ {{TearsSmall}}연사 +0.25"
+			.."#↑ {{DamageSmall}}공격력 +0.25"
+			.."#↑ {{LuckSmall}}행운 +1"
+			.."#{{LunarHC}} 악마방이 공월방으로 으로 대체될 확률 x2"
+			.."#{{LunarHC}} {{ColorGray}}공월방은 거래 아이템의 등급이 높으나 거래 시 {{BrokenHeart}}가 추가됩니다."
+			.."{{CR}}",
+			QuoteDesc = "수제 모찌",
+		},
+		[mod.SolarItems.Wormhole] = {
+			Name = "",
+			Description = ""
+			.."#!!! 미구현"
 			.."{{CR}}",
 			QuoteDesc = "",
 		},
-		[mod.SolarItems.HyperDice] = {
-			Name = "아브라함의 지도",
+		[mod.SolarItems.Friend] = {
+			Name = "",
 			Description = ""
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."{{CR}}",
-			QuoteDesc = "",
-		},
-		[mod.SolarItems.HyperDice] = {
-			Name = "아브라함의 지도",
-			Description = ""
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."{{CR}}",
-			QuoteDesc = "",
-		},
-		[mod.SolarItems.HyperDice] = {
-			Name = "아브라함의 지도",
-			Description = ""
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."{{CR}}",
-			QuoteDesc = "",
-		},
-		[mod.SolarItems.HyperDice] = {
-			Name = "아브라함의 지도",
-			Description = ""
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."{{CR}}",
-			QuoteDesc = "",
-		},
-		[mod.SolarItems.HyperDice] = {
-			Name = "아브라함의 지도",
-			Description = ""
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."{{CR}}",
-			QuoteDesc = "",
-		},
-		[mod.SolarItems.HyperDice] = {
-			Name = "아브라함의 지도",
-			Description = ""
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."{{CR}}",
-			QuoteDesc = "",
-		},
-		[mod.SolarItems.HyperDice] = {
-			Name = "아브라함의 지도",
-			Description = ""
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."{{CR}}",
-			QuoteDesc = "",
-		},
-		[mod.SolarItems.HyperDice] = {
-			Name = "아브라함의 지도",
-			Description = ""
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."{{CR}}",
-			QuoteDesc = "",
-		},
-		[mod.SolarItems.HyperDice] = {
-			Name = "아브라함의 지도",
-			Description = ""
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."{{CR}}",
-			QuoteDesc = "",
-		},
-		[mod.SolarItems.HyperDice] = {
-			Name = "아브라함의 지도",
-			Description = ""
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."{{CR}}",
-			QuoteDesc = "",
-		},
-		[mod.SolarItems.HyperDice] = {
-			Name = "아브라함의 지도",
-			Description = ""
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."{{CR}}",
-			QuoteDesc = "",
-		},
-		[mod.SolarItems.HyperDice] = {
-			Name = "아브라함의 지도",
-			Description = ""
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."{{CR}}",
-			QuoteDesc = "",
-		},
-		[mod.SolarItems.HyperDice] = {
-			Name = "아브라함의 지도",
-			Description = ""
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."{{CR}}",
-			QuoteDesc = "",
-		},
-		[mod.SolarItems.HyperDice] = {
-			Name = "아브라함의 지도",
-			Description = ""
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."{{CR}}",
-			QuoteDesc = "",
-		},
-		[mod.SolarItems.HyperDice] = {
-			Name = "아브라함의 지도",
-			Description = ""
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."{{CR}}",
-			QuoteDesc = "",
-		},
-		[mod.SolarItems.HyperDice] = {
-			Name = "아브라함의 지도",
-			Description = ""
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."{{CR}}",
-			QuoteDesc = "",
-		},
-		[mod.SolarItems.HyperDice] = {
-			Name = "아브라함의 지도",
-			Description = ""
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."{{CR}}",
-			QuoteDesc = "",
-		},
-		[mod.SolarItems.HyperDice] = {
-			Name = "아브라함의 지도",
-			Description = ""
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."{{CR}}",
-			QuoteDesc = "",
-		},
-		[mod.SolarItems.HyperDice] = {
-			Name = "아브라함의 지도",
-			Description = ""
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."{{CR}}",
-			QuoteDesc = "",
-		},
-		[mod.SolarItems.HyperDice] = {
-			Name = "아브라함의 지도",
-			Description = ""
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."{{CR}}",
-			QuoteDesc = "",
-		},
-		[mod.SolarItems.HyperDice] = {
-			Name = "아브라함의 지도",
-			Description = ""
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."{{CR}}",
-			QuoteDesc = "",
-		},
-		[mod.SolarItems.HyperDice] = {
-			Name = "아브라함의 지도",
-			Description = ""
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."{{CR}}",
-			QuoteDesc = "",
-		},
-		[mod.SolarItems.HyperDice] = {
-			Name = "아브라함의 지도",
-			Description = ""
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."{{CR}}",
-			QuoteDesc = "",
-		},
-		[mod.SolarItems.HyperDice] = {
-			Name = "아브라함의 지도",
-			Description = ""
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."{{CR}}",
-			QuoteDesc = "",
-		},
-		[mod.SolarItems.HyperDice] = {
-			Name = "아브라함의 지도",
-			Description = ""
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."{{CR}}",
-			QuoteDesc = "",
-		},
-		[mod.SolarItems.HyperDice] = {
-			Name = "아브라함의 지도",
-			Description = ""
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."{{CR}}",
-			QuoteDesc = "",
-		},
-		[mod.SolarItems.HyperDice] = {
-			Name = "아브라함의 지도",
-			Description = ""
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."{{CR}}",
-			QuoteDesc = "",
-		},
-		[mod.SolarItems.HyperDice] = {
-			Name = "아브라함의 지도",
-			Description = ""
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."{{CR}}",
-			QuoteDesc = "",
-		},
-		[mod.SolarItems.HyperDice] = {
-			Name = "아브라함의 지도",
-			Description = ""
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
-			.."#"
+			.."#!!! 미구현"
 			.."{{CR}}",
 			QuoteDesc = "",
 		},
 		--#endregion
 	}
+
+	--unimplemented
+	CollectibleDesc[mod.SolarItems.SunGlasses] = CollectibleDesc[mod.SolarItems.Wormhole]
+	CollectibleDesc[mod.SolarItems.Jupiter] = CollectibleDesc[mod.SolarItems.Wormhole]
+	CollectibleDesc[mod.SolarItems.Scepter] = CollectibleDesc[mod.SolarItems.Wormhole]
+	CollectibleDesc[mod.SolarItems.Dial+0] = CollectibleDesc[mod.SolarItems.Wormhole]
+	CollectibleDesc[mod.SolarItems.Dial+1] = CollectibleDesc[mod.SolarItems.Wormhole]
+	CollectibleDesc[mod.SolarItems.Dial+2] = CollectibleDesc[mod.SolarItems.Wormhole]
+	CollectibleDesc[mod.SolarItems.Dial+3] = CollectibleDesc[mod.SolarItems.Wormhole]
+	CollectibleDesc[mod.SolarItems.Dial+4] = CollectibleDesc[mod.SolarItems.Wormhole]
+	CollectibleDesc[mod.SolarItems.Dial+5] = CollectibleDesc[mod.SolarItems.Wormhole]
+	CollectibleDesc[mod.SolarItems.Dial+6] = CollectibleDesc[mod.SolarItems.Wormhole]
+	CollectibleDesc[mod.SolarItems.Dial+7] = CollectibleDesc[mod.SolarItems.Wormhole]
+	CollectibleDesc[mod.SolarItems.Dial+8] = CollectibleDesc[mod.SolarItems.Wormhole]
+	CollectibleDesc[mod.SolarItems.Dial+9] = CollectibleDesc[mod.SolarItems.Wormhole]
+	CollectibleDesc[mod.SolarItems.CatFish] = CollectibleDesc[mod.SolarItems.Wormhole]
+	CollectibleDesc[mod.SolarItems.AdInfinitum] = CollectibleDesc[mod.SolarItems.Wormhole]
+	CollectibleDesc[mod.SolarItems.Whistle] = CollectibleDesc[mod.SolarItems.Wormhole]
+
+	--internal only
+	CollectibleDesc[mod.SolarItems.CursedHead] = CollectibleDesc[mod.SolarItems.Friend]
+	CollectibleDesc[mod.SolarItems.CursedBody] = CollectibleDesc[mod.SolarItems.Friend]
+	CollectibleDesc[mod.SolarItems.CursedSoul] = CollectibleDesc[mod.SolarItems.Friend]
 
 	local TrinketDesc = {
 		--#region
@@ -709,7 +534,7 @@ if HeavensCall then
 			.."#{{Collectible582}} 화면이 어지러워집니다."
 			.."{{CR}}",
 			Horse = ""
-			.."#{{Collectible582}} 화면이 어지러워집니다."
+			.."#{{Collectible582}} 화면이 {{ColorYellow}}더 크게{{CR}} 어지러워집니다."
 			.."{{CR}}",
 			QuoteDesc = "왜곡된 시야",
 		},
